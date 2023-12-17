978e3231c978308efe50b44f16d51a668834564d
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-17 14:04:12 -0800

    lets sleep

--- pizza.py
+++ pizza.py
@@ -2,6 +2,7 @@
 In a file called pizza.py, implement a program that expects exactly one command-line argument, the name (or path) of a CSV file in Pinocchio’s format, and outputs a table formatted as ASCII art using tabulate, a package on PyPI at pypi.org/project/tabulate. Format the table using the library’s grid format. If the user does not specify exactly one command-line argument, or if the specified file’s name does not end in .csv, or if the specified file does not exist, the program should instead exit via sys.exit.
 
 """
+# you should use regular.csv or sicilian.csv
 
 import sys
 import csv
