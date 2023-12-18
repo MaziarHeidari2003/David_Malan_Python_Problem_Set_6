f912884da5daef4c12f14687262aa0e6bd45ecbd
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-18 09:14:30 -0800

    end of the third question

--- scourgify.py
+++ scourgify.py
@@ -8,21 +8,17 @@ def main():
     with open(sys.argv[1]) as file:
       reader = csv.DictReader(file)
       for row in reader:
-        split_name = (row['name']).split(',')
-        output.append({'first':split_name[1].lstrip() , 'last':split_name[0] , 'house': row['house'].lstrip()})  
-  except FileNotFoundError:
-    sys.exit('File not found')
+        split_name = row['name'].split(',')
+        output.append({'first':split_name[1] , 'last': split_name[0] , 'house': row['house']})
 
+  except FileNotFoundError:   
+    sys.exit('File not found')   
 
   with open(sys.argv[2], 'w') as file :
-    writer = csv.DictWriter(file, fieldnames = ["first", "last" , "house"])
-    writer.writerow({'first': 'first', 'last':'last', 'house': 'house'})
-    for row in output :
-      writer.writerow({'first':row['first'] , 'last':row['last'] , 'house': row['house']}) 
-
-
-
-
+    writer = csv.DictWriter(file, fieldnames= ['first','last','house'])
+    writer.writerow({'first': 'first', 'last':'last' , 'house':'house'})
+    for row in output:
+      writer.writerow({'first':row['first'] , 'last': row['last'] , 'house': row['house']})
 
 def command_check():
   if len(sys.argv) > 3 :
@@ -32,8 +28,6 @@ def command_check():
   else:
     if not '.csv' in sys.argv[1] or not '.csv' in sys.argv[2]:
       sys.exit('Not a CSV file')  
-
-
 
 
 if __name__ == '__main__' :
